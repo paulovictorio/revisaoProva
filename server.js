a96e5a5d462cb42d1dd1dbb9e04cfa25787fe971
Paulo <paulocesarvictorio940@gmail.com>
2024-10-17 16:35:10 -0300

    Revisão Docker

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 require("./app/routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
